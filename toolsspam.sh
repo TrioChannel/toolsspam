bb7fc5b715b7e34392b8fa68aa6a932ba78a5d59
TrioChannel <72772686+TrioChannel@users.noreply.github.com>
2020-11-10 23:14:05 +0700

    Create toolsspam.sh

---
+++ toolsspam.sh
@@ -0,0 +1,45 @@
+#usr/bin/bash
+#mau ngapain broo
+#anak anjing
+#"\033[31m hahaha"Merah
+#"\033[32m hahahah"hijau
+#"\033[33m hahaha"kuning
+#"\033[34m bshsjs"purpel
+#"\033[35m bsbs"pink
+#"\033[36m whhwh"cyanclear
+clear
+sleep 1
+echo "\033[34mAs""\033[34msall""\033[34ma""\033[34mm""\033[34mu""\033[34mal""\033[34ma""\033[34mikum""\033[34m""\033[34m wr""\033[33m.""\033[34mwb""\033[34m Se""\033[34mlamat""\033[34m Datang"
+echo "\033[36m BY""\033[36m TR""\033[36mIO""\033[36m CHANNNEL""\033[36m  Semoga""\033[36m  Bermanfaat"
+sleep 5
+clear
+echo "\033[34m╔═══════════════════════════════════════════════════╗"
+echo "\033[34m║""\033[32m Youtube""\033[33m :""\033[31m TRIO CHANNEL""\033[34m                            ║"
+echo "\033[34m║""\033[32m Github""\033[33m :""\033[31m https://github.com/TrioChannel""\033[34m           ║"
+echo "\033[34m║""\033[32m SUBCRIBE""\033[33m•""\033[31mLIKE""\033[33m•""\033[33mSHARE""\033[34m           ║"
+echo "\033[34m╚═══════════════════════════════════════════════════╝"
+echo
+echo "\033[31m^︵^""\033[33m Silahkan Di Pilih Broo"
+echo "\033[35m────────────────────────┐"
+echo "\033[33m[1],Spam Call,SmS,Whatsapp Unlimited"
+echo "\033[33m[2],Spam Call,Sms Unlimited"
+echo
+echo "\033[33mTRIO CHANNEL"
+   read -p "Silahkan Pilih Spamnya :" pil :
+
+#Tahap 1 Smksksksslosjsbdvdjdbdvddjdhbddjdhbddjfvdjbdrirbekbeirbdorbdodbdomdbdjfhirdjbdbdhdheidhdbidbdidbdudbdudhdidbdudvdidhdidhdihdidhdidhdidndjdhdhddjdkkbddhhdudbdududhdhdbdjbdb
+if [ $pil = 1 ]
+then
+git clone https://github.com/AbilSeno/12Spammers
+cd 12Spammers
+python2 main.py
+fi
+#Tahap 2 sjdjdjjdjdjdjdjdjddjdjdjdjjdhddytdtdgdgdgdgdgdgdgdgdtdgddggddgdgdgdggdgddtdgdgdgstststsysysyysysysysyssysyysysysys BY TRIO CHANNEL
+if [ $pil = 2 ]
+then
+git clone https://github.com/AbilSeno/MDSpammers
+cd MDSpammers
+python2 main.py
+fi
+
+#Awassss
